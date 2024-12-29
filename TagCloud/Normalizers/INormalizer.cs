ea9307c3609291e70bf399aa7c6b8d9f68232029
Zakhar <zax_2000@mail.ru>
2024-12-29 21:33:47 +0500

    Релизован функционал нормализации частоты уникальных слов.

--- TagCloud/Normalizers/INormalizer.cs
+++ TagCloud/Normalizers/INormalizer.cs
@@ -3,6 +3,9 @@
     // Интерфейс нормализации количества каждого слова
     internal interface INormalizer
     {
-        public Dictionary<string, double> Normalize(Dictionary<string, uint> values);
+        public Dictionary<string, double> Normalize(
+            Dictionary<string, uint> values,
+            double minCoefficient = 0.25,
+            int decimalPlaces = 4);
     }
 }
