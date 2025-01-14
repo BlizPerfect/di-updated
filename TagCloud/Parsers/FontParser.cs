b99e3acbf58f5d051842155d41fe3c6604b5929d
Zakhar <zax_2000@mail.ru>
2025-01-14 10:21:16 +0500

    Рефакторинг кода FontParser.
    FontParser теперь возвращает FontFamily

--- TagCloud/Parsers/FontParser.cs
+++ TagCloud/Parsers/FontParser.cs
@@ -4,15 +4,14 @@ namespace TagCloud.Parsers
 {
     internal static class FontParser
     {
-        public static string ParseFont(string font)
+        public static FontFamily ParseFont(string font)
         {
             if (!FontFamily.Families.Any(
                 x => x.Name.Equals(font, StringComparison.OrdinalIgnoreCase)))
             {
                 throw new ArgumentException($"Неизвестный шрифт {font}");
             }
-            return font;
+            return new FontFamily(font);
         }
-
     }
 }
